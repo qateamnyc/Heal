5e4002cf8159b34e944ec38b20e3dda82a159de3
Masuma <masuma.akter.nyc@gmail.com>
2023-08-17 13:40:22 -0400

    c

--- BootCamp-HealthFirst/src/test/java/heal/com/pageElements/heal_Log_in_pageElements.java
+++ BootCamp-HealthFirst/src/test/java/heal/com/pageElements/heal_Log_in_pageElements.java
@@ -4,7 +4,27 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class heal_Log_in_pageElements {
+	@FindBy(xpath = "//*[@id=\"masthead\"]/div[2]/div[3]/ul/li[4]/a/span[1]")
+	public WebElement loginbutton;
 	
+	@FindBy(xpath = "//*[@id=\"masthead\"]/div[2]/div[3]/ul/li[4]/ul/a[1]")
+	public  WebElement memberportal;
+
+	@FindBy(xpath = "//*[@id=\"username\"]")
+	public WebElement username;
+	
+	@FindBy(xpath = "//*[@id=\"password\"]")
+	public WebElement password;
+
+	@FindBy(xpath = "//*[@id=\"ReactAppWrapper-wide\"]/div/div[2]/form/div[4]/button")
+	public  WebElement login;
+
+	@FindBy(xpath = "//*[@id=\"ReactAppWrapper-wide\"]/div[3]/div[2]/div[2]/form/div[2]/a")
+	public  WebElement skipbutton;
+
+	@FindBy(xpath = "//*[@id=\"ReactAppWrapper-wide\"]/div[3]/div/div[3]/div/div[2]/div/div/div/button[1]")
+	public WebElement nothanks;
+		
 	
 	
 	
--- BootCamp-HealthFirst/src/test/java/heal/com/pageElements/heal_accountcreate_pageElements.java
+++ BootCamp-HealthFirst/src/test/java/heal/com/pageElements/heal_accountcreate_pageElements.java
@@ -1,10 +1,49 @@
 package heal.com.pageElements;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class heal_accountcreate_pageElements {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	@FindBy(xpath = "//*[@id=\"masthead\"]/div[2]/div[3]/ul/li[4]/a/span[1]")
+	public WebElement loginbutton;
+	
+	@FindBy(xpath = "//*[@id=\"masthead\"]/div[2]/div[3]/ul/li[4]/ul/a[1]")
+	public  WebElement memberportal;
 
-	}
+	@FindBy(xpath = "//*[@id=\"ReactAppWrapper-wide\"]/div/div[2]/div[2]/p[1]/a")
+	public WebElement CreateAccount;
+	
+	@FindBy(xpath = "//*[@id=\"ReactAppWrapper-wide\"]/div/div/div[2]/div[1]/div[5]/div/div[2]/div")
+	public WebElement ActivateCoverage;
+
+	@FindBy(xpath = "//*[@id=\"ReactAppWrapper-wide\"]/div/div/div[2]/div[1]/button")
+	public  WebElement Continuebutton;
+
+	@FindBy(xpath = "//*[@id=\"ReactAppWrapper-wide\"]/div/div/div[2]/div[1]/div[2]/div[2]/input")
+	public  WebElement FirstName;
+
+	@FindBy(xpath = "//*[@id=\"ReactAppWrapper-wide\"]/div/div/div[2]/div[1]/div[2]/div[3]/input")
+	public WebElement DateOfBirth;
+
+	@FindBy(xpath = "//*[@id=\"mobile\"]")
+	public WebElement MobileNumber;
+
+	@FindBy(xpath = "//*[@id=\"ReactAppWrapper-wide\"]/div/div/div[2]/div[1]/div[2]/div[6]/input")
+	public WebElement EmailAddress;
+
+	@FindBy(xpath = "//*[@id=\"ReactAppWrapper-wide\"]/div/div/div[2]/div[1]/div[2]/div[7]/input")
+	public WebElement ReenterEmail;
+	
+	@FindBy(xpath = "//*[@id=\"selectCheckbox\"]")
+	public WebElement selectackkowlgecheckbox ;
+
+	@FindBy(xpath = "//*[@id=\"ReactAppWrapper-wide\"]/div/div/div[2]/div[1]/div[3]/button")
+	public WebElement clickcontinue ;
+	
+	@FindBy(xpath = "//*[@id=\"ReactAppWrapper-wide\"]/div[1]/div/div[2]/div/div[2]/div/span[1]")
+	public WebElement TickEmailMe ;
+
+
 
 }
